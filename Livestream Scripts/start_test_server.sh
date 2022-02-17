f45fe32ab08984f7757f6c9f2c5c9a685c8f033e
Rodrigo Becerril Ferreyra <becerrilferreyra.rodrigo@gmail.com>
2022-02-17 13:27:23 -0800

    Add livestream test server

---
+++ Livestream Scripts/start_test_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! $# == 1 ]
+    echo "Usage: $0 filename"
+    exit
+fi
+
+ffmpeg  -re -i /home/pi/Projects/LivestreamServer/Carlito.mkv \
+    -c copy -g 30 -sc_threshold 0 \
+    -f tee -map 0:v -map 0:a \
+    "/dev/shm/hls/streamVOD.mp4|[f=hls:hls_time=6:hls_list_size=5:method=PUT]/dev/shm/hls/live.m3u8"
