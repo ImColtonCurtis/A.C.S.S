c14c493184a02f76db17f737d2b2884438394aae
Rodrigo Becerril Ferreyra <becerrilferreyra.rodrigo@gmail.com>
2022-02-18 14:07:31 -0800

    Fix parameter issue in test server script

--- Livestream Scripts/start_test_server.sh
+++ Livestream Scripts/start_test_server.sh
@@ -5,7 +5,7 @@ if [ ! $# == 1 ]
     exit
 fi
 
-ffmpeg  -re -i /home/pi/Projects/LivestreamServer/Carlito.mkv \
+ffmpeg  -re -i "/home/pi/Projects/LivestreamServer/$0" \
     -c copy -g 30 -sc_threshold 0 \
     -f tee -map 0:v -map 0:a \
     "/dev/shm/hls/streamVOD.mp4|[f=hls:hls_time=6:hls_list_size=5:method=PUT]/dev/shm/hls/live.m3u8"
